1402024585b4eda91d53d8879761e6ef8975b1d7
LongNhat0808 <90303680+LongNhat0808@users.noreply.github.com>
2021-09-09 16:21:46 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,24 +1,82 @@
-const userscore = 0;
-const computerscore = 0;
+let userscore = 0;
+let computerscore = 0;
 const userscore_span = document.getElementById("user-score");
 const computerscore_span = document.getElementById("computer-score");
-const scoreboard_div = document.querySelector("score-board");
-const result_div = document.querySelector("result");
+const scoreboard_div = document.querySelector(".score-board");
+const result_p = document.querySelector(".result > p");
 const bua_div = document.getElementById("b");
 const keo_div = document.getElementById("k");
 const bao_div = document.getElementById("g");
 
-bua_div.addEventListener('click', function() {
-  game(b);
-  console.log("hey you clicked on Bua");
-})
+function getComputChoice() {
+  const Choices = ['b', 'k', 'g'];
+  const randomNumber = Math.floor(Math.random() * 3);
+  return Choices[randomNumber];
+}
+function ConvertToWord(letter) {
+  if (letter === "b") return "Búa";
+  if (letter === "g") return "Bao";
+  return "Kéo";
+}
 
-keo_div.addEventListener('click', function() {
-  game(k);
-  console.log("hey you clicked on Keo");
-})
+function win(userChoice, computerchoice) {
+  userscore++ ;
+  userscore_span.innerHTML =  userscore;
+  computerscore_span.innerHTML = computerscore;
+  const smalluser = "người chơi".fontsize(3).sup();
+  const smallcomp = "máy".fontsize(3).sup();
+  result_p.innerHTML = ConvertToWord(userChoice) + smalluser +  " đánh bại " + ConvertToWord(computerchoice) + smallcomp + ". Bạn thắng !! ";
+  const userChoice_div = document.getElementById(userChoice);
+  userChoice_div.classList.add('green-glow');
+  setTimeout(function() { userChoice_div.classList.remove('green-glow')}, 2000);
+}
+function lose(userChoice, computerchoice) {
+  computerscore++ ;
+  userscore_span.innerHTML =  userscore;
+  computerscore_span.innerHTML = computerscore;
+  const smalluser = "người chơi".fontsize(3).sup();
+  const smallcomp = "máy".fontsize(3).sup();
+  result_p.innerHTML = ConvertToWord(userChoice) + smalluser +  " bị đánh bại bởi " + ConvertToWord(computerchoice) + smallcomp + ". Bạn thua !! ";
+  const userChoice_div = document.getElementById(userChoice);
+  userChoice_div.classList.add('red-glow');
+  setTimeout(() => userChoice_div.classList.remove('red-glow'), 2000);
+}
+function draw(userChoice, computerchoice) {
+  const smalluser = "người chơi".fontsize(3).sup();
+  const smallcomp = "máy".fontsize(3).sup();
+  result_p.innerHTML = ConvertToWord(userChoice) + smalluser +  " ngang nhau với " + ConvertToWord(computerchoice) + smallcomp + ". Hòa nhau !! ";
+  const userChoice_div = document.getElementById(userChoice);
+  userChoice_div.classList.add('gray-glow');
+  setTimeout(() => userChoice_div.classList.remove('gray-glow'), 2000);
+}
 
-bao_div.addEventListener('click', function() {
-  bao(g);
-  console.log("hey you clicked on Bao");
-})
+function game(userChoice) {
+  const computerchoice = getComputChoice();
+  switch (userChoice + computerchoice) {
+    case 'bb':
+    case 'kk':
+    case 'gg':
+    draw(userChoice, computerchoice);
+      break;
+    case 'bg':
+    case 'kb':
+    case 'gk':
+    lose(userChoice, computerchoice);
+      break;
+    case 'gb':
+    case 'bk':
+    case 'kg':
+    win(userChoice, computerchoice);
+      break;
+  }
+}
+
+function main() {
+  bua_div.addEventListener('click', () => game("b"));
+
+  keo_div.addEventListener('click', () => game("k"));
+
+  bao_div.addEventListener('click', () => game("g"));
+}
+
+main();
